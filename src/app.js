025d041b51d73a4f68939b34a4deb7a2d680a823
ChrigorG <chrigor81@gmail.com>
2023-08-02 23:00:32 -0400

    add express

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
+const express = require('express');
 const axios = require('axios');
 // File system
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
+
 
 
 const cert = fs.readFileSync(
@@ -22,50 +23,61 @@ const credentials = Buffer.from(
     `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
 ).toString('base64');
 
-axios({
-    method: 'POST',
-    url: `${process.env.GN_ENDPOINT}/oauth/token`,
-    headers: {
-        Authorization: `Basic ${credentials}`,
-        'Content-Type': 'application/json'
-    },
-    httpsAgent: agent,
-    data: {
-        grant_type: 'client_credentials'
-    }
-}).then((result) => {
-    const accessToken = result.data?.access_token; // => ? optional chaining
-    const urlCharge = '/v2/cob';
 
-    const reqGN = axios.create({
-        baseURL: process.env.GN_ENDPOINT,
-        httpsAgent: agent,
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+app.get('/', (req, res) => {
+    axios({
+        method: 'POST',
+        url: `${process.env.GN_ENDPOINT}/oauth/token`,
         headers: {
-            Authorization: `Bearer ${accessToken}`,
+            Authorization: `Basic ${credentials}`,
             'Content-Type': 'application/json'
+        },
+        httpsAgent: agent,
+        data: {
+            grant_type: 'client_credentials'
         }
-    });
-
-    const endpoint = `${process.env.GN_ENDPOINT}/v2/cob`;
-
-    const dataCob = {
-        calendario: {
-          expiracao: 3600
-        },
-        devedor: {
-          cpf: '12345678909',
-          nome: 'Francisco da Silva'
-        },
-        valor: {
-          original: '12.45'
-        },
-        chave: '068a706d-e46d-4aff-afbf-450c751ad5ee',
-        solicitacaoPagador: 'Cobrança dos serviços prestados.'
-    };
-
-    reqGN.post(urlCharge, dataCob).then((result) => {
-        console.log(result.data);
-    });
-
+    }).then((result) => {
+        const accessToken = result.data?.access_token; // => ? optional chaining
+        const urlCharge = '/v2/cob';
+    
+        const reqGN = axios.create({
+            baseURL: process.env.GN_ENDPOINT,
+            httpsAgent: agent,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        const dataCob = {
+            calendario: {
+              expiracao: 3600
+            },
+            devedor: {
+              cpf: '12345678909',
+              nome: 'Francisco da Silva'
+            },
+            valor: {
+              original: '12.45'
+            },
+            chave: '068a706d-e46d-4aff-afbf-450c751ad5ee',
+            solicitacaoPagador: 'Cobrança dos serviços prestados.'
+        };
+    
+        reqGN.post(urlCharge, dataCob).then((result) => {
+            res.send(result.data);
+        });
+    });    
 });
+
+
+app.listen(8000, () => {
+    console.log('Server only API PIX');
+});
+
 
