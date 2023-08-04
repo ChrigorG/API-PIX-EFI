72a1b854d6f70a2e7111ce636c5dc7714a7690a5
ChrigorG <chrigor81@gmail.com>
2023-08-03 20:01:58 -0400

    refactor for new file afi.js

--- src/app.js
+++ src/app.js
@@ -2,61 +2,21 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const express = require('express');
-const axios = require('axios');
-// File system
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
-
-
-
-const cert = fs.readFileSync(
-    path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
-);
-
-const agent = new https.Agent({
-    pfx: cert,
-    passphrase: ''
-});
-
-const credentials = Buffer.from(
-    `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
-).toString('base64');
-
+const GNRequest = require('./apis/afi');
 
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
-app.get('/', async(req, res) => {
-    const authResponse = await axios({
-        method: 'POST',
-        url: `${process.env.GN_ENDPOINT}/oauth/token`,
-        headers: {
-            Authorization: `Basic ${credentials}`,
-            'Content-Type': 'application/json'
-        },
-        httpsAgent: agent,
-        data: {
-            grant_type: 'client_credentials'
-        }
-    });
+const reqGNAlready = GNRequest.GNRequest();
 
-    const accessToken = authResponse.data?.access_token; // => ? optional chaining
+app.get('/', async(req, res) => {
+    const reqGN = await reqGNAlready;
     const urlCharge = '/v2/cob';
     const urlQRCode = (idLoc) => {
         return `/v2/loc/${idLoc}/qrcode`;
     };
-
-    const reqGN = axios.create({
-        baseURL: process.env.GN_ENDPOINT,
-        httpsAgent: agent,
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    });
 
     const dataCob = {
         calendario: {
